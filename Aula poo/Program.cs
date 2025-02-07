b8755bc908e2616c3f1719ebcb0d7cc85c08c6a0
cauazinm <cauazinm@LYNXSTATION>
2025-02-07 09:06:08 -0300

    Adicionar arquivos de projeto.

---
+++ Aula poo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a largura :");
+
+            int largura = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do retângulo:");
+
+            int altura = int.Parse(Console.ReadLine());
+
+            Retangulo retangulo = new Retangulo(largura, altura);
+
+            retangulo.Exibir();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula poo/Retangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_poo
+{
+    
+        class Retangulo
+        {
+            
+            public int Largura { get; set; }
+            public int Altura { get; set; }
+
+          
+            public Retangulo(int largura, int altura)
+            {
+                Largura = largura;
+                Altura = altura;
+            }
+
+          
+            public void Exibir()
+            {
+                for (int i = 0; i < Altura; i++)
+                {
+                    for (int j = 0; j < Largura; j++)
+                    {
+                    if (i == 0 || i == Altura - 1 || j == 0 || j == Largura - 1)
+                    {
+                        Console.Write("X");
+                    }
+                    else
+                    {
+                        Console.Write(" "); 
+                    }
+                }
+                    Console.WriteLine(); 
+                }
+            }
+        }
+   }
+
